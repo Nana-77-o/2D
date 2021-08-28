21a5e0468d2b5ccb35767e2bd040ba17421bb843
Nana_77 <nana77amery@gmail.com>
2021-08-28 09:49:43 +0900

    最初のコミット

---
+++ Assets/C#/TochiInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TochiInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Touchi.Began += onTouchBegan;
+        Touchi.Ended += (info) =>
+        {
+            Debug.Log("ボタンが離された時" + info.screenPoint);
+        };
+    }
+
+    void onTouchBegan(TouchInfo info)
+    {
+        Debug.Log("ボタンが押された時" + info.screenPoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
